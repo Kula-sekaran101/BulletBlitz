c36bbd2652fc4805877592bff2014b76266f3dc7
Kula-sekaran101 <kulasekaran2005@gmail.com>
2025-09-22 18:36:46 +0530

    GameInstance

--- Source/BulletBlitz/GameInstance/BulletBlitzGameInstance.cpp
+++ Source/BulletBlitz/GameInstance/BulletBlitzGameInstance.cpp
@@ -1,19 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BulletBlitzGameInstance.h"
-
-UBulletBlitzGameInstance::UBulletBlitzGameInstance()
-{
-	RoomCode = TEXT("");
-}
-
-void UBulletBlitzGameInstance::SetRoomCode(const FString& NewRoomCode)
-{
-	RoomCode = NewRoomCode;
-}
-
-FString UBulletBlitzGameInstance::GetRoomCode() const
-{
-	return RoomCode;
-}
+//#include "BulletBlitzGameInstance.h"
+//
+//UBulletBlitzGameInstance::UBulletBlitzGameInstance()
+//{
+//	RoomCode = TEXT("");
+//}
+//
+//void UBulletBlitzGameInstance::SetRoomCode(const FString& NewRoomCode)
+//{
+//	RoomCode = NewRoomCode;
+//}
+//
+//FString UBulletBlitzGameInstance::GetRoomCode() const
+//{
+//	return RoomCode;
+//}
--- Source/BulletBlitz/GameInstance/BulletBlitzGameInstance.h
+++ Source/BulletBlitz/GameInstance/BulletBlitzGameInstance.h
@@ -1,27 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Engine/GameInstance.h"
-#include "BulletBlitzGameInstance.generated.h"
-
-/**
- * 
- */
-UCLASS()
-class BULLETBLITZ_API UBulletBlitzGameInstance : public UGameInstance
-{
-	GENERATED_BODY()
-public:
-	UBulletBlitzGameInstance();
-
-    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Room")
-    FString RoomCode;
-
-    UFUNCTION(BlueprintCallable, Category = "Room")
-    void SetRoomCode(const FString& NewRoomCode);
-
-    UFUNCTION(BlueprintCallable, Category = "Room")
-    FString GetRoomCode() const;
-};
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Engine/GameInstance.h"
+//#include "BulletBlitzGameInstance.generated.h"
+//
+//
+//
+//UCLASS()
+//class BULLETBLITZ_API UBulletBlitzGameInstance : public UGameInstance
+//{
+//	GENERATED_BODY()
+//public:
+//	UBulletBlitzGameInstance();
+//
+//    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Room")
+//    FString RoomCode;
+//
+//    UFUNCTION(BlueprintCallable, Category = "Room")
+//    void SetRoomCode(const FString& NewRoomCode);
+//
+//    UFUNCTION(BlueprintCallable, Category = "Room")
+//    FString GetRoomCode() const;
+//};
